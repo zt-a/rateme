5efdcc7bab9ac36da12c3be55458fc002c35c6dc
zt-a <zt20061113@gmail.com>
2026-03-22 17:35:18 +0600

    fix: hardcode vercel URL in CORS

--- backend/main.py
+++ backend/main.py
@@ -28,9 +28,10 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://rateme-teal.vercel.app",
     settings.FRONTEND_URL,
+    "*",
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
