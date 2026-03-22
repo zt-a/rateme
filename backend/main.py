08dcdd3e35d8c85f0da254d332d286bc3ef8a0ba
zt-a <zt20061113@gmail.com>
2026-03-22 17:04:24 +0600

    Set cors

--- backend/main.py
+++ backend/main.py
@@ -46,6 +46,8 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://rateme-teal.vercel.app",
+    "https://rateme-production.up.railway.app"
 ]
 
 app.add_middleware(
